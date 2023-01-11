15e7707faab0575062543486b454058ed95f92c4
Msp88 <aleast.88@gmail.com>
2023-01-11 21:27:14 +0300

    Cards initialization

---
+++ src/main/java/com/rustprojects/loans/repository/LoansRepository.java
@@ -0,0 +1,15 @@
+package com.rustprojects.loans.repository;
+
+import java.util.List;
+
+import com.rustprojects.loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+	
+	List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
